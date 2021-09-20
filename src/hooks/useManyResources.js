923bbb25c5630610aeb499edbbc259ca0c33de2e
Elise Muellerleile <eliamue@live.com>
2021-09-19 23:56:42 -0500

    hook files added, useManyResources hook written

---
+++ src/hooks/useManyResources.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import { fetchManyResources } from '../services/apiService';
+
+export const useManyResources = () => {
+  const [loading, setLoading] = useState(true);
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    fetchManyResources()
+      .then((res) => setResources(res))
+      .finally(() => setLoading(false));
+  });
+  return { loading, resources };
+};
