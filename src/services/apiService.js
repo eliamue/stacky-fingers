a3dfaeb666a72cf5b4d4386ccd03b01263643c27
Elise Muellerleile <eliamue@live.com>
2021-09-19 23:53:58 -0500

    fetchOneResource added to apiservice

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -5,3 +5,25 @@ export const fetchAllResources = async () => {
 
   return results;
 };
+
+export const fetchOneResource = async (resourceId) => {
+  const res = await fetch(`https://salty-forest-33057.herokuapp.com/${resourceId}`);
+
+  const { id, src_name, category, src_description, st_address, city, us_state, zip, phone, text_num, website, email, is_24_7 } = await res.json();
+
+  return {
+    id,
+    src_name,
+    category,
+    src_description,
+    st_address,
+    city,
+    us_state,
+    zip,
+    phone,
+    text_num,
+    website,
+    email,
+    is_24_7
+  };
+};
